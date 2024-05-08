fa60115159a135fd94a93879d029f734cb17e4bc
serg3295 <serg3295@gmail.com>
2024-05-08 07:31:52 +0300

    chore(deps): update serialport to v12.0.0

---
+++ esbuild.mjs
@@ -0,0 +1,24 @@
+import * as esbuild from "esbuild"
+
+const prod = process.argv[2] === "production"
+const options = {
+  entryPoints: ["./bin/nodemcu-tool.js"],
+  bundle: true,
+  format: "cjs",
+  minify: prod ? true : false,
+  sourcemap: prod ? false : true,
+  platform: "node",
+  target: "es6", //"node18",
+  outfile: "./out/nodemcu-tool.js",
+  external: [
+    "@serialport/bindings-cpp/prebuilds",
+  ],
+}
+
+if (process.argv[2] === "watch") {
+  let ctx = await esbuild.context(options)
+  console.log("watching...")
+  ctx.watch
+} else {
+  await esbuild.build(options)
+}
