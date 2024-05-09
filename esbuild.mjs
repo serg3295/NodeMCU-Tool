da381fbd8cb027adfa28dadc423e823a210996d3
serg3295 <serg3295@gmail.com>
2024-05-09 06:44:01 +0300

    fix linter's  warnings

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,6 +1,6 @@
-import * as esbuild from "esbuild"
+import * as esbuild from "esbuild";
 
-const prod = process.argv[2] === "production"
+const prod = process.argv[2] === "production";
 const options = {
   entryPoints: ["./bin/nodemcu-tool.js"],
   bundle: true,
@@ -10,15 +10,16 @@ const options = {
   platform: "node",
   target: "es2022",
   outfile: "./out/nodemcu-tool.js",
-  external: [
-    "@serialport/bindings-cpp/prebuilds",
-  ],
-}
+  external: ["@serialport/bindings-cpp/prebuilds"],
+};
 
-if (process.argv[2] === "watch") {
-  let ctx = await esbuild.context(options)
-  console.log("watching...")
-  ctx.watch
-} else {
-  await esbuild.build(options)
-}
+(async () => {
+  if (process.argv[2] === "watch") {
+    const ctx = await esbuild.context(options);
+    await ctx.watch();
+    // eslint-disable-next-line no-console
+    console.log("ESBuild watching...");
+  } else {
+    await esbuild.build(options);
+  }
+})();
