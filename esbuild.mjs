de6434297ef9f9f591fc1db2c784265c37cbe9d7
serg3295 <serg3295@gmail.com>
2024-06-28 10:39:01 +0300

    build: improve esbuild.mjs

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,25 +1,58 @@
+/* eslint-disable no-console */
 import * as esbuild from "esbuild";
 
-const prod = process.argv[2] === "production";
-const options = {
-  entryPoints: ["./bin/nodemcu-tool.js"],
-  bundle: true,
-  format: "cjs",
-  minify: prod ? true : false,
-  sourcemap: prod ? false : true,
-  platform: "node",
-  target: "es2022",
-  outfile: "./out/nodemcu-tool.js",
-  external: ["@serialport/bindings-cpp/prebuilds"],
+const production = process.argv.includes("production");
+const watch = process.argv.includes("watch");
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: "esbuild-problem-matcher",
+
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd(result => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
 };
 
-(async () => {
-  if (process.argv[2] === "watch") {
-    const ctx = await esbuild.context(options);
+async function main() {
+  const ctx = await esbuild.context({
+    entryPoints: ["./bin/nodemcu-tool.js"],
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "node",
+    target: "es2022",
+    outfile: "./out/nodemcu-tool.js",
+    external: ["@serialport/bindings-cpp/prebuilds"],
+    logLevel: "silent",
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin,
+    ],
+  });
+  if (watch) {
     await ctx.watch();
-    // eslint-disable-next-line no-console
-    console.log("ESBuild watching...");
   } else {
-    await esbuild.build(options);
+    await ctx.rebuild();
+    await ctx.dispose();
   }
-})();
+}
+
+main().catch(e => {
+  console.error(e);
+  throw new Error(e);
+});
