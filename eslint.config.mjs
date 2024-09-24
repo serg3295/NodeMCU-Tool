5b59c51bdd3ee10c65a4189b4d325f5367997d8e
serg3295 <serg3295@gmail.com>
2024-09-24 09:38:34 +0300

    style: change eslint rules; add prettierrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,9 @@ import js from "@eslint/js";
 import globals from "globals";
 import stylistic from "@stylistic/eslint-plugin";
 
-
 export default [
   js.configs.recommended,
+  stylistic.configs["recommended-flat"],
 
   {
     languageOptions: {
@@ -21,80 +21,26 @@ export default [
   {
     rules: {
       ...js.configs.recommended.rules,
-      "no-console": "warn",
-      "@stylistic/no-extra-semi": ["off"],
+
+      "@stylistic/indent": ["error", 4],
+      "@stylistic/no-extra-semi": ["error"],
       "@stylistic/semi": ["warn", "always"],
-      "@stylistic/quotes": ["error", "single", {"avoidEscape": true}],
-      "@stylistic/brace-style": ["error", "1tbs"],
-      "@stylistic/function-call-spacing": ["error", "never"],
-      "@stylistic/arrow-parens": ["warn", "as-needed"],
+      "@stylistic/quotes": ["error", "single", { avoidEscape: true }],
+      "@stylistic/arrow-parens": ["warn", "as-needed"], // This rule enforces parentheses around arrow function parameters regardless of arity.
+      "@stylistic/brace-style": ["error", "1tbs"], // This rule enforces consistent brace style for blocks.
+      "@stylistic/operator-linebreak": ["error", "after", { overrides: { "?": "before", ":": "before" } }],
+      "@stylistic/indent-binary-ops": "off",
 
-      // from eslint-config-aenondynamics:
+      "no-console": "warn",
+      "no-useless-return": "warn",
+      "no-return-assign": "error",
+      "no-implicit-coercion": "warn",
+      "yoda": ["error", "never"],
 
-      // Possible Errors
-      "no-await-in-loop": 1,
-
-      // Best Practices
-      "block-scoped-var": 2,
-      "class-methods-use-this": 2,
-      "default-case": 2,
-      "no-alert": 2,
-      "no-caller": 2,
-      "no-eval": 2,
-      "no-extend-native": 2,
-      "no-floating-decimal": 2,
-      "no-implicit-globals": 2,
-      "no-implied-eval": 2,
-      "no-invalid-this": 2,
-      "no-iterator": 2,
-      "no-labels": 2,
-      "no-lone-blocks": 2,
-      "no-loop-func": 2,
-      "no-new": 2,
-      "no-new-func": 2,
-      "no-new-wrappers": 2,
-      "no-param-reassign": 1,
-      "no-proto": 2,
-      "no-return-assign": 2,
-      "no-return-await": 2,
-      "no-self-compare": 2,
-      "no-sequences": 2,
-      "no-throw-literal": 2,
-      "no-unmodified-loop-condition": 2,
-      "no-unused-expressions": 2,
-      "no-useless-call": 2,
-      "no-useless-concat": 1,
-      "no-useless-escape": 1,
-      "no-useless-return": 2,
-      "no-void": 2,
-      "no-with": 2,
-      "prefer-promise-reject-errors": 2,
-      "require-await": 2,
-      "vars-on-top": 2,
-      "wrap-iife": [2, "outside"],
-      yoda: 2,
-
-      // Node.js and CommonJS
-      "no-sync": 2,
-      "global-require": 2,
-      "handle-callback-err": 2,
-      "no-path-concat": 2,
-      "no-process-exit": 2,
-      "no-new-require": 2,
-      "no-mixed-requires": 2,
-
-      // Variables
-      "no-use-before-define": 2,
-      "no-shadow": 2,
-      "no-shadow-restricted-names": 2,
-      "no-undefined": 2,
-
-      // ECMAScript 6
-      "no-confusing-arrow": 2,
-      "no-var": 2,
-      "prefer-const": 2,
-      "prefer-rest-params": 2,
-      "rest-spread-spacing": [2, "never"],
+      "no-shadow": "error",
+      "no-undefined": "error",
+      "no-use-before-define": "error",
+      // "no-useless-assignment": "error",
     },
   },
 
@@ -103,10 +49,10 @@ export default [
     languageOptions: {
       sourceType: "module",
     },
-    "rules": {
-      "@stylistic/quotes": ["error", "double", {"avoidEscape": true}],
-    }
-
+    rules: {
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+    },
   },
 
   {
@@ -114,5 +60,5 @@ export default [
     languageOptions: {
       sourceType: "commonjs",
     },
-  }
+  },
 ];
